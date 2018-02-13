e41b952574dc460fa03186cf610be12ce0311ff3
Pjotreq <36454290+Pjotreq@users.noreply.github.com>
2018-02-14 00:00:27 +0100

    1.day - HelloWorld, TypesAndVariables

---
+++ HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld {
+    //skolsky priklad
+    public static void main (String[] args){
+        System.out.println("Hello World!!!");
+    }
+
+}
---
+++ TypesAndVariables.java
@@ -0,0 +1,12 @@
+public class TypesAndVariables {
+    public static void main (String[] args) {
+        int myVariable;
+        myVariable = 5;
+
+        System.out.println("Value of myVariable is " + myVariable);
+        myVariable = 10;
+        System.out.println("Value of myVariable is " + myVariable);
+        myVariable = myVariable + 1 + 3;
+        System.out.println("Value of myVariable is " + myVariable );
+    }
+}
